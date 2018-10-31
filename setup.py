92808eb0f6f2b819887637c87d23add8537a41fe
Will Boxx <wboxx1@gmail.com>
2018-10-31 09:19:55 -0500

    file reorganization

--- setup.py
+++ setup.py
@@ -5,17 +5,17 @@ with open("README.rst", "r") as fh:
 
 setuptools.setup(
     name="intermod_library",
-    version="0.0.1",
+    version="0.1.0",
     author="Will Boxx",
     author_email="wboxx1@gmail.com",
-    description="A library of tools for finding and viewing intermodulation prducts and harmonics",
+    description="A library of tools for finding and viewing intermodulation products and harmonics",
     long_description=long_description,
     long_description_content_type="text/x-rst",
     url="https://github.com/wboxx1/intermod-library",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License:: OSI Approved:: GNU General Public License v3(GPLv3)",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
         "Operating System :: OS Independent",
     ],
 )
