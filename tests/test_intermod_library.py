e4f4ad1b4263ce8486c9a4491aa7b3f6f1350ff2
Will Boxx <wboxx1@gmail.com>
2018-11-05 22:14:31 -0600

    added travis.yml and tox.ini

---
+++ tests/test_intermod_library.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+import intermod_library.intermod_tools as it
+
+def test_intermod_table():
+    signals = [1000, 2000]
+    order = 3
+    table = it.intermod_table(signals, order)
+    table.head()
