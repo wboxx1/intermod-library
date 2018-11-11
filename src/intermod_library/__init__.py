76e71acaf4662d2441a6f3d5a30946e97ef8692b
Will Boxx <wboxx1@gmail.com>
2018-11-11 16:12:16 -0600

    version fixed

--- src/intermod_library/__init__.py
+++ src/intermod_library/__init__.py
@@ -3,5 +3,5 @@
 """Top-level package for intermod-library."""
 
 __author__ = """William Boxx"""
-__email__ = 'wboxx1@gmail.com'
-__version__ = '0.1.1'
+__email__ = "wboxx1@gmail.com"
+__version__ = "0.1.1"
