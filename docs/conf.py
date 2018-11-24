882cf730941e8f6a5fa6d49094a60351a51f459c
Will Boxx <wboxx1@gmail.com>
2018-11-24 14:00:32 -0600

    docs config adit

--- docs/conf.py
+++ docs/conf.py
@@ -20,9 +20,8 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../src/'))
-
 import intermod_library
+sys.path.insert(0, os.path.abspath('../src/'))
 
 # -- General configuration ---------------------------------------------
 
@@ -158,6 +157,3 @@ texinfo_documents = [
      'One line description of project.',
      'Miscellaneous'),
 ]
-
-
-
